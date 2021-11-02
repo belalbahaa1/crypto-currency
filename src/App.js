9b975d7681bcbf47f8bbaf36bdf4b39a07b118df
belalbahaa1 <60116768+belalbahaa1@users.noreply.github.com>
2021-11-02 04:59:44 +0200

    crypto

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { makeStyles } from "@material-ui/core";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Coinpage from "./Pages/Coinpage";
+import Homepage from "./Pages/Homepage";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path="/" exact component={Homepage} />
+        <Route path="/coins/:id" component={Coinpage} />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,72 @@
+import {
+  AppBar,
+  Button,
+  Container,
+  createTheme,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles, ThemeProvider } from "@material-ui/styles";
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Raleway",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#fff",
+    },
+    type: "dark",
+  },
+});
+
+const Header = () => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const { currency, setCurrency } = CryptoState();
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => history.push("/")}
+              className={classes.title}
+              variant="h6"
+            >
+              Crypto Currency
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+              value={currency}
+              onClick={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={"INR"}>INR</MenuItem>
+              <MenuItem value={"USD"}>USD</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default Header;
